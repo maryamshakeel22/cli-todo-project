cd0659f67072ad40b941cac285e962e85164dfce
Maryam Shakeel <157610901+maryamshakeel22@users.noreply.github.com>
2024-05-23 21:01:27 +0500

    Add files via upload

---
+++ todo.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+let todos: string[]= []
+let condition = true
+
+while(condition)
+
+{let todoQuestion = await inquirer.prompt(
+    [
+    {
+        name:"firstQuestion",
+        type:"input",
+        message:"What would you like to add in you todos?"
+    },
+    {
+        name:"secondQuestion",
+        type:"confirm",
+        message:"Would you like to add more in your todos?",
+        Default:"true"
+    }
+]
+)
+todos.push(todoQuestion.firstQuestion);
+console.log(todos);
+condition = todoQuestion.secondQuestion
+}
